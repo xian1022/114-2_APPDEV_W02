2081e05d8678048ab14295d6aaa8f583d1a19620
xian1022 <C112181113@nkust.edu.tw>
2026-03-05 11:08:11 +0800

    new

---
+++ W02_ex01/ex02.java
@@ -0,0 +1,13 @@
+public class ex02 {
+    public static void main(String[] args) {
+        // alt + shift +f 自動整理code
+    int grade = 80;
+    char type = 'm';
+    if ( grade >= 60 && type == 'm' ) {
+    System.out.println("課程: " + type +"\n成績及格: " + grade);
+    }
+    else {
+    System.out.println("課程不正確或成績不及格");
+    }
+}
+}
---
+++ W02_ex01/helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        int grade = 80;
+        if (grade > 60) {
+            System.out.println("成績及格!\n 分數：" + grade);
+        }
+    }
+}
